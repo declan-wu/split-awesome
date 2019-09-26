66690f4bae439c1ca61bf3dd3bfa3e7fbed2727d
Declan Wu <declan.s.wu@gmail.com>
2019-09-26 11:20:13 -0400

    added tests folder test files

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -4,8 +4,8 @@ import os
 class BaseConfig:
     """Base configuration"""
     TESTING = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  
-
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'my_precious' 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
