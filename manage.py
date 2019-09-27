0bce5b19d5d491cb091cf6b7c13167e1f340b69a
Declan Wu <declan.s.wu@gmail.com>
2019-09-27 00:44:22 -0400

    add migrations to gitignore

--- manage.py
+++ manage.py
@@ -11,3 +11,7 @@ manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
+
+# heroku run python manage.py db init --app split-awesome
+# heroku run python manage.py db migrate --app split-awesome
+# heroku run python manage.py db upgrade --app split-awesome
